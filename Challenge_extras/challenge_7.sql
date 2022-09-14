5ab239a48afc5b3670000c90043df1ee09d27c2f
kevin daniliuk <108556672+DaniliukK95@users.noreply.github.com>
2022-09-14 17:04:36 -0400

    final uploads

--- Challenge_extras/challenge_7.sql
+++ Challenge_extras/challenge_7.sql
@@ -7,7 +7,7 @@ SELECT e.emp_no,
 	ti.title, 
 	ti.from_date, 
 	ti.to_date
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees AS e
 INNER JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
@@ -20,7 +20,7 @@ SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
 rt.title
-INTO unique_titles
+--INTO unique_titles
 FROM retirement_titles AS rt
 WHERE rt.to_date = '9999-01-01'
 ORDER BY rt.emp_no ASC, rt.to_date DESC;
@@ -30,10 +30,14 @@ ORDER BY rt.emp_no ASC, rt.to_date DESC;
 --Using COUNT to create a table with the number of
 --retirement-age employees by most recent job title. 
 SELECT COUNT(ut.title), ut.title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
+
+SELECT * FROM retiring_titles;
+
+
 
 --deliverable 2
 --create a table that holds the employees who 
@@ -42,7 +46,7 @@ SELECT DISTINCT ON (emp_no) e.emp_no,
 e.first_name, e.last_name, e.birth_date, 
 de.from_date, de.to_date, 
 ti.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees AS e 
 INNER JOIN dept_emp AS de 
 ON (e.emp_no = de.emp_no)
@@ -52,9 +56,18 @@ WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
 ORDER BY e.emp_no;
 
-----------
+SELECT * FROM mentorship_eligibility;
 
---additional query/table 1
+
+
+-------------------------
+
+
+
+
+DROP TABLE retiring_titles_2;
+
+--other queries
 -- use hire date 1985-1988 as a filter. 
 SELECT e.emp_no, e.first_name, e.last_name, e.hire_date,
 	ti.title, ti.from_date, ti.to_date
@@ -74,7 +87,7 @@ WHERE rt2.to_date = '9999-01-01'
 ORDER BY rt2.emp_no ASC, rt2.to_date DESC;
 
 SELECT COUNT(ut2.title), ut2.title
-INTO retiring_titles_with_hire_date
+--INTO retiring_titles_2
 FROM unique_titles_2 AS ut2
 GROUP BY ut2.title
 ORDER BY COUNT DESC;
@@ -82,7 +95,32 @@ ORDER BY COUNT DESC;
 
 
 
---additional query/table 2
+
+
+
+
+--total employed employees
+SELECT e.emp_no, e.first_name, e.last_name, 
+	ti.title, ti.from_date, ti.to_date
+INTO retirement_titles_3
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+ORDER BY e.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt3.emp_no) rt3.emp_no,
+rt3.first_name, rt3.last_name, rt3.title
+--INTO unique_titles_3
+FROM retirement_titles_3 AS rt3
+WHERE rt3.to_date = '9999-01-01'
+ORDER BY rt3.emp_no ASC, rt3.to_date DESC;
+
+
+
+
+
+
 --elinas query
 SELECT DISTINCT ON(ut.title, d.dept_name)
 ut.title, d.dept_name, 
